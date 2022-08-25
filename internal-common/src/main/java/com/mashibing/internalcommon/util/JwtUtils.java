3129f7839416b05b7c574f94dfe7a2751efc56e8
xuchuangye <59134197+xuchuangye@users.noreply.github.com>
2022-08-25 19:53:51 +0800

    token的解析

--- internal-common/src/main/java/com/mashibing/internalcommon/util/JwtUtils.java
+++ internal-common/src/main/java/com/mashibing/internalcommon/util/JwtUtils.java
@@ -3,6 +3,8 @@ package com.mashibing.internalcommon.util;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -15,17 +17,25 @@ import java.util.Map;
  */
 public class JwtUtils {
 
-	private static final String SIGH = "2agharrawe&#$";
+	public static final String SIGN = "ggagahagraiha&$@&*@";
+	public static final String JWT_KEY = "passengerPhone";
 
-	public static String generatorToken(Map<String, String> map) {
+	/**
+	 * 生成token
+	 * @param passengerPhone 乘客手机号
+	 * @return
+	 */
+	public static String generatorToken(String passengerPhone) {
+		Map<String, String> map = new HashMap<>();
+		map.put(JWT_KEY, passengerPhone);
+
 		//设置token过期时间
 		Calendar calendar = Calendar.getInstance();
 		//增加1天时间
 		calendar.add(Calendar.DATE, 1);
-
 		Date date = calendar.getTime();
-		JWTCreator.Builder builder = JWT.create();
 
+		JWTCreator.Builder builder = JWT.create();
 		//整合Map
 		map.forEach(
 				builder::withClaim
@@ -34,17 +44,28 @@ public class JwtUtils {
 		//整合过期时间
 		builder.withExpiresAt(date);
 		//生成token
-		String sign = builder.sign(Algorithm.HMAC256(SIGH));
+		String sign = builder.sign(Algorithm.HMAC256(SIGN));
 
 		//返回token
 		return sign;
 	}
 
-	public static void main(String[] args) {
-		Map<String, String> map = new HashMap<>();
-		map.put("name", "zhangsan");
-		map.put("age", "18");
-		String s = generatorToken(map);
-		System.out.println("生成的token: " + s);
+	/**
+	 * 解析token
+	 * @param token 令牌
+	 * @return
+	 */
+	public static String parseToken(String token) {
+		DecodedJWT verify = JWT.require(Algorithm.HMAC256(SIGN)).build().verify(token);
+		Claim claim = verify.getClaim(JWT_KEY);
+		return claim.toString();
 	}
+
+	public static void main(String[] args) {
+		String token = generatorToken("13875823291");
+		System.out.println("生成的token: " + token);
+
+		System.out.println("解析的token：" + parseToken(token));
+	}
+
 }
