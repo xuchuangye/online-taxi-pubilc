bef630e4797e2db3f2f312711b3cc1ccc06c54e1
xuchuangye <59134197+xuchuangye@users.noreply.github.com>
2022-08-25 15:24:17 +0800

    1.7 version 插入用户信息

--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
@@ -6,6 +6,7 @@ import com.mashibing.servicepassengeruser.mapper.PassengerUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,11 +28,15 @@ public class UserService {
 		map.put("passenger_phone", passengerPhone);
 		List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
 		if (passengerUsers == null || passengerUsers.size() == 0) {
-			System.out.println("无记录");
-		}else {
-			System.out.println(passengerUsers.get(0).getPassengerPhone());
-			System.out.println(passengerUsers.get(0).getPassengerName());
-			System.out.println(passengerUsers.get(0).getPassengerGender());
+			PassengerUser passengerUser = new PassengerUser();
+			passengerUser.setPassengerPhone(passengerPhone);
+			passengerUser.setPassengerName("张三");
+			passengerUser.setPassengerGender((byte) 0);
+			passengerUser.setState((byte) 0);
+			LocalDateTime now = LocalDateTime.now();
+			passengerUser.setGmtCreate(now);
+			passengerUser.setGmtModified(now);
+			passengerUserMapper.insert(passengerUser);
 		}
 		//判断乘客手机号是否存在
 
