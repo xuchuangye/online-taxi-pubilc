7cc9cebeee8ca3e9beddcc1e4fb087d8b26cba56
xuchuangye <59134197+xuchuangye@users.noreply.github.com>
2022-09-05 15:14:48 +0800

    service-driver-user服务的注册到Nacos并且服务发现

--- service-driver-user/src/main/java/com/mashibing/servicedriveruser/ServiceDriverUserApplication.java
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/ServiceDriverUserApplication.java
@@ -3,12 +3,14 @@ package com.mashibing.servicedriveruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author xcy
  * @date 2022/9/5 - 9:33
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.mashibing.servicedriveruser.mapper")
 public class ServiceDriverUserApplication {
 
